4e50a4f93aae71efed16db110fbb0f47a384b5be
OnahProsperity <prosperauthor@gmail.com>
2022-07-07 14:16:16 +0100

    created eth client

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"context"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
@@ -9,7 +10,10 @@ var infura = "https://mainnet.infura.io/v3/5aa06cf6c9d54a79bc1ce4f8ca6cfa6d"
 
 func main() {
 
-	ethclient.DialContext(context.Background(), infura)
-	// fmt.Println(a)
-
+	client, err := ethclient.DialContext(context.Background(), infura)
+	if err != nil {
+		fmt.Println("error logged during connection:", err)
+	}
+	// in other to avoid a memory lick
+	defer client.Close()
 }
