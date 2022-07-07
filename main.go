adbec4bfd69312bae3f7c0b5ecacefd5d4b97bb7
OnahProsperity <prosperauthor@gmail.com>
2022-07-07 16:16:35 +0100

    func to check user balance

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"math/big"
+	"math"
+	"github.com/ethereum/go-ethereum/common"
 	"log"
 	"os"
 	"context"
@@ -9,6 +12,7 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+var ctx = context.Background()
 
 // use godot package to load/read the .env file and
 // return the value of the key
@@ -25,20 +29,44 @@ func goDotEnvVariable(key string) string {
   
 func main() {
 	getBlockNumber()
+	getBalanceAtBlock()
 }
 
 func getBlockNumber() {
 	dotenv := goDotEnvVariable("INFURAURL")
-	client, err := ethclient.DialContext(context.Background(), dotenv)
+	client, err := ethclient.DialContext(ctx, dotenv)
 	if err != nil {
 		fmt.Println("error logged during connection:", err)
 	}
 	// in other to avoid a memory lick
 	defer client.Close()
 
-	blockNum, err := client.BlockByNumber(context.Background(), nil)
+	blockNum, err := client.BlockByNumber(ctx, nil)
 	if err != nil {
 		fmt.Println("error attempting block num:", err)
 	}
 	fmt.Println("Current Block Number: ", blockNum.Number())
+}
+
+func getBalanceAtBlock() {
+	dotenv := goDotEnvVariable("INFURAURL")
+	client, err := ethclient.DialContext(ctx, dotenv)
+	if err != nil {
+		fmt.Println("error logged during connection:", err)
+	}
+	// in other to avoid a memory lick
+	defer client.Close()
+
+	addr := "0xab3B229eB4BcFF881275E7EA2F0FD24eeaC8C83a"
+	address := common.HexToAddress(addr)
+
+	balance, err := client.BalanceAt(ctx, address, nil)
+	if err != nil {
+		fmt.Println("error attempting to get balance num:", err)
+	}
+	FBalance := new(big.Float)
+	FBalance.SetString(balance.String())
+
+	value := new(big.Float).Quo(FBalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println("Current user balance is: ", value)
 }
