eb846e92ddb92170ace4fc68653660b2780dc54f
OnahProsperity <prosperauthor@gmail.com>
2022-07-07 14:37:05 +0100

    get last block

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"context"
+	"fmt"
+
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-var infura = "https://mainnet.infura.io/v3/5aa06cf6c9d54a79bc1ce4f8ca6cfa6d"
+var infuraURL = "wss://mainnet.infura.io/ws/v3/5aa06cf6c9d54a79bc1ce4f8ca6cfa6d"
 
 func main() {
-
-	client, err := ethclient.DialContext(context.Background(), infura)
+	// cxt := context.Background()
+	client, err := ethclient.DialContext(context.Background(), infuraURL)
 	if err != nil {
 		fmt.Println("error logged during connection:", err)
 	}
 	// in other to avoid a memory lick
 	defer client.Close()
+
+	blockNum, err := client.BlockByNumber(context.Background(), nil)
+	if err != nil {
+		fmt.Println("error attempting block num:", err)
+	}
+	fmt.Println(blockNum.Number())
 }
